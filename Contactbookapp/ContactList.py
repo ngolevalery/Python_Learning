8d3cd8a407f9f645c13ecc64166df39465750c4a
ngolevalery <valery.ngole07@gmail.com>
2024-10-30 11:34:17 +0100

    'contact_list'

---
+++ Contactbookapp/ContactList.py
@@ -0,0 +1,44 @@
+import allfunctions as allf
+import time
+
+print("----------CONTACT LIST----------")
+
+print("Please select an option to continue...\n")
+
+contact_app_open = True
+
+while contact_app_open:
+    print("Enter 1 to Add a new contact...")
+    print("Enter 2 to View all contacts...")
+    print("Enter 3 to Edit a contact...")
+    print("Enter 4 to Delete a contact...")
+    print("Enter 0 to Exit program...\n")
+
+    #taking input from user
+    action = input("What do you want to do? ")
+
+    #a list of all action choices
+    options = ["0","1","2","3","4"]
+
+    #checkin user input to perform required action
+
+    if action not in (options):
+        print("\nInvalid option !!!")
+        action = input("Enter again: ") 
+
+    if action == "1":
+        allf.add_contact() #calling the add_contacts function
+        print("Contact was successfully added !")
+    elif action == "2":
+        allf.view_contacts() #calling the view_contact function
+    elif action == "3":
+        allf.edit_contact() #calling the edit_contact function
+    elif action == "4":
+        allf.delete_contact() #Delete contact fxn
+    elif action == "0":
+        print("exiting...")
+        time.sleep(2)
+        contact_app_open = False
+
+
+    print("--------------------")
---
+++ Contactbookapp/allfunctions.py
@@ -0,0 +1,72 @@
+import csv
+
+contact_list = []
+
+
+#functions to write to csv.
+def update_csv(contactlist = "x"):
+    with open("contaclist.csv","w",newline="") as f:
+        write = csv.writer(f, quoting=csv.QUOTE_ALL)
+        for contact in contact_list:
+            write.writerow(contact)
+            break
+        print("contact list file has been updated")
+
+#FUNCTION TO ADD CONTACTS
+def add_contact(contact_name = "x",phone_number = "y"):
+    contact_name = input("\nEnter Contact Name: ").capitalize()
+    phone_number = input("Enter Phone Number:  ")
+    contact_list.append([contact_name,phone_number])
+    update_csv(contact_list)
+    return contact_list
+
+
+#FUNCTION TO VIEW ALL CONTACTS
+def view_contacts():
+    print("----------Your Contacts---------")
+    print("NAME        PHONE NUMBER")
+    for contact in contact_list:
+        print("---------------------------")
+        print(contact)
+
+#FUNCTION TO EDIT CONTACTS
+def edit_contact(name = "x"):
+    name = input("Please enter the name of the contact you want to edit: ").capitalize()
+    if name in contact_list:
+        print(contact_list[name])
+    else:
+        print("could not find name")
+        return
+    action_control = input("Are you sure you want to edit this contact? Enter y or n ").lower()
+    if action_control == "y":
+        new_number = input(f"enter {name}'s new number: ")
+        contact_list.append[name]
+        print(f"{name}'s phone number has been updated to {contact_list[name]}")
+        update_csv()
+    elif action_control == "n":
+        print("Action Cancelled !")
+    else:
+        print("invalid input! Could not complete action")
+        return
+    
+#FUNCTION TO DELETE CONTACTS
+def delete_contact(name="x"):
+    name = input("Enter the name of the contact you want to delete: ").capitalize()
+    if name in contact_list:
+        action_control = input(f"Are you sure you want to delete {name}'s contact ? y/n: ").lower()
+        if action_control == "y":
+            contact_list.remove(name)
+            print("Contact successfully deleted")
+        elif action_control == "n":
+            print("action cancelled")
+            return
+        else:
+            print("invalid option")
+            return
+    else:
+        print("contact not found !")
+        return
+
+
+
+
